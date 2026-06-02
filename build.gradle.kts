73b292705cd876ef97ce7f1facb134d52de93af8
umblox <39423646+umblox@users.noreply.github.com>
2026-06-02 16:28:40 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
+// Project-level build file
 plugins {
-    alias(libs.plugins.android.application) version "8.2.2" apply false
-    alias(libs.plugins.kotlin.android) version "1.9.22" apply false
-    alias(libs.plugins.devtools.ksp) version "1.9.22-1.0.17" apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
 }
-
