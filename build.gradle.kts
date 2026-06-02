45f4d6ae75b95bd519853529b304e84a9db94b00
umblox <39423646+umblox@users.noreply.github.com>
2026-06-02 15:50:32 +0700

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    alias(libs.plugins.android.application) version "8.2.2" apply false
+    alias(libs.plugins.kotlin.android) version "1.9.22" apply false
+    alias(libs.plugins.devtools.ksp) version "1.9.22-1.0.17" apply false
+}
+
