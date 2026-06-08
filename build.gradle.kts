c166964ebc3b86fcb74550df44cb61f9eb95cd6e
umblox <39423646+umblox@users.noreply.github.com>
2026-06-08 18:18:27 +0700

    Add Google Services plugin to build.gradle.kts
    Added Google Services plugin to project-level build file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
-// Project-level build file
+// Proyek-level build file (Root)
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+    // SUNTIKKAN PLUGIN GOOGLE SERVICES DI LEVEL ROOT
+    id("com.google.gms.google-services") version "4.4.1" apply false
 }
