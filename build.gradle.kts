b2cf58866f5a2eef44568a37f90f9b2ad4ef99fc
umblox <39423646+umblox@users.noreply.github.com>
2026-06-09 21:45:55 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
-    // SUNTIKKAN PLUGIN GOOGLE SERVICES DI LEVEL ROOT
+    // 🔥 FIX UTAMA: Plugin KSP dihapus murni untuk mempercepat waktu inisialisasi Gradle
     id("com.google.gms.google-services") version "4.4.1" apply false
 }
