c4481dbcbe307b01f5b863438b0b9ec10253d5af
umblox <39423646+umblox@users.noreply.github.com>
2026-06-16 01:07:30 +0700

    Correct JitPack repository URL formatting
    Fix formatting of JitPack repository URL in settings.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ dependencyResolutionManagement {
         mavenCentral()
         // ALAMAT BARU: Menyuruh server mencari pustaka grafik MPAndroidChart di sini
         maven { url = java.net.URI("https://jitpack.io")
-        maven { url = uri("https://www.jitpack.io") }
+        maven { url = uri("https://www.jitpack.io") }}
     }
 }
 
