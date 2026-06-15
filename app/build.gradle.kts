6bd8fa6a818b98dc3c3b4b85280c8e77f3224490
umblox <39423646+umblox@users.noreply.github.com>
2026-06-16 00:57:05 +0700

    Add biometric library dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.biometric:biometric:1.1.0")
 
     // Navigation & Lifecycle Dropdown UI
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
