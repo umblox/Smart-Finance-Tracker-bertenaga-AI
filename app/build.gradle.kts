9421e03efa5d71212ea9726bd30f9b6cd63c2227
umblox <39423646+umblox@users.noreply.github.com>
2026-07-29 19:03:48 +0700

    Bump version code and version name to 3.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         minSdk = 26
         targetSdk = 34
         
-        versionCode = 2
-        versionName = "2.0.1"
+        versionCode = 3
+        versionName = "3.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
